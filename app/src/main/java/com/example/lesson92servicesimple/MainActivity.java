3ba8d6bf390e2c740d1687f82540c29dfdccf7b6
Sukhovolskij.Andrew <sukhovolskijandrewoid@gmail.com>
2019-11-21 00:07:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lesson92servicesimple/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.lesson92servicesimple;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStart(View v) {
+        startService(new Intent(this, MyService.class));
+    }
+
+    public void onClickStop(View v) {
+        stopService(new Intent(this, MyService.class));
+    }
+}
